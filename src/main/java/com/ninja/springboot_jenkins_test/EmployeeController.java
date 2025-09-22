ca6687d23cd2bb2e6e59fef068411a15690e59de
santoshkumar711 <santoshkumar711711@gmail.com>
2025-09-22 18:26:13 +0530

    controller add

--- src/main/java/com/ninja/springboot_jenkins_test/EmployeeController.java
+++ src/main/java/com/ninja/springboot_jenkins_test/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController {
 	@RequestMapping("/test1")
 	public String test1()
 	{
-		return "this is jenkins test3 ";
+		return "this is jenkins test4 ";
 	}
 	
 	@RequestMapping("/save")
